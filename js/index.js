41736f1ab968f8501526e5dd842b53d15b0783cc
Juan Garcia <35821855+jgarc132@users.noreply.github.com>
2026-09-01 19:28:40 -0600

    Added projects with css

--- js/index.js
+++ js/index.js
@@ -66,6 +66,34 @@ messageForm.addEventListener("submit", function(event){
     messageForm.reset();
 });
 
+const projectSection = document.getElementById("projects");
+const projectList = projectSection.querySelector("ul");
+ 
+fetch("https://api.github.com/users/jgarc132/repos")
+  .then(response => response.json())
+  .then(function(repositories) {
+    for (let i = 0; i < repositories.length; i++) {
+      const project = document.createElement("li");
+      project.textContent = repositories[i].name;
+      projectList.appendChild(project);
+    }
+  })
+  .catch(error => console.error("Error:", error));
 
+/*
+async function addRepos(){
+    try{
+        const result = await fetch("https://api.github.com/users/jgarc132/repos");
+        const repositories = await result.json();
+          for (let i = 0; i < repositories.length; i++) {
+            const project = document.createElement("li");
+            project.textContent = repositories[i].name;
+            projectList.appendChild(project);
+        }
+    }catch(error){
+        console.log(error);
+    }
+}
+    */
 
-
+addRepos();
